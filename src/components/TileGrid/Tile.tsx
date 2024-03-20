90dfceb13e44e15144b6b82882686f1d4a5b27c4
Luca Kooistra <luca.kooistra@avivasolutions.nl>
2024-03-20 17:07:50 +0100

    give every tile a pair

--- src/components/TileGrid/Tile.tsx
+++ src/components/TileGrid/Tile.tsx
@@ -1,15 +1,22 @@
 type TileProps = {
   onClick: () => void;
   visible: boolean;
-}
+  icons: JSX.Element[];
+};
 
-export function Tile({
-  onClick,
-  visible
-}:TileProps) {
+export function Tile({ onClick, visible, icons }: TileProps) {
   return (
     <>
-        <li onClick={onClick} className={`tilegrid__item ${visible ? 'tilegrid__item-selected' : 'tilegrid__item-hidden'}`}></li>
+      <li
+        onClick={onClick}
+        className={`tilegrid__item ${
+          visible ? "tilegrid__item-selected" : "tilegrid__item-hidden"
+        }`}
+      >
+        {icons.map((icon, index) => (
+          <div key={index}>{icon}</div>
+        ))}
+      </li>
     </>
   );
 }
--- src/components/TileGrid/TileGrid.tsx
+++ src/components/TileGrid/TileGrid.tsx
@@ -1,17 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Tile } from "./Tile";
+import { Chance } from "chance";
+import { TileIcons } from "../TileIcons/TileIcons";
 import "./tilegrid.scss";
 
 export function TileGrid() {
   const [tileVisibility, setTileVisibility] = useState<boolean[]>(
     Array(16).fill(false)
   ); // Array van 16 tegels, allemaal onzichtbaar in het begin
+  const [iconPairs, setIconPairs] = useState<JSX.Element[]>([]);
 
   const handleTileClick = (id: number) => {
     const newVisibility = [...tileVisibility]; // Maak een kopie van de zichtbaarheidsarray
     newVisibility[id] = !newVisibility[id]; // Wijzig de zichtbaarheid van de tegel
     setTileVisibility(newVisibility); // Werk de staat bij
   };
+
+  useEffect(() => {
+    const icons = TileIcons();
+    const iconPairs = icons.concat(icons); // Verdubbel de array met iconen
+    const mixedIconPairs = Chance().shuffle(iconPairs); // Randomiseer de iconen
+
+    setIconPairs(mixedIconPairs);
+  }, []);
 
   return (
     <>
@@ -22,6 +33,7 @@ export function TileGrid() {
               key={id}
               onClick={() => handleTileClick(id)} // Gebruik de nieuwe handleClick-functie
               visible={tileVisibility[id]} // Geef de zichtbaarheid door aan de tegel
+              icons={[iconPairs[id]]} // Geef elk icoonpaar door aan een tegel
             />
           ))}
         </ul>
